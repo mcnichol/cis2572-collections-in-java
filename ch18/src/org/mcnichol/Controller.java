71ce253ef0f5d93b978854eb66c70022ef76b47f
mcnichol <mcnichol.m@gmail.com>
2021-02-07 23:41:28 -0600

    Update gitignore to ignore out build folder

---
+++ ch18/src/org/mcnichol/Controller.java
@@ -0,0 +1,19 @@
+package org.mcnichol;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    void getFileCount() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
